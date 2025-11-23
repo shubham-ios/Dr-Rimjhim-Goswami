2dbf89e628b33da14e6b67f339eb4d139ed8992f
Shubham Sharma <shubhamsharma33@gmail.com>
2025-11-23 21:46:28 +0530

    feat: remove detailed clinic address from Home page

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -207,7 +207,7 @@ const Home: React.FC = () => {
           ></iframe>
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-xl shadow-xl max-w-xs text-center">
             <h4 className="font-serif text-sage text-xl mb-2">Visit Our Clinic</h4>
-            <p className="text-charcoal/70 text-sm">Shop 311/3,<br />Chattarpur Temple Main Road, <br />Opposite to MCD Park, <br />Chhatarpur, New Delhi, <br />Delhi 110074</p>
+            {/* <p className="text-charcoal/70 text-sm">Chhatarpur, New Delhi</p> */}
             <p className="text-charcoal/70 text-sm mb-4">Open Mon-Sun, 10:00 am - 8:00 pm</p>
             <Link to="https://maps.app.goo.gl/Y36TEtkoGeWRkSmo9" className="text-rose text-sm font-bold uppercase tracking-wider">Get Directions</Link>
           </div>
